1ac78dc7781bc4d7807bfe897ce5ed8dcdd47f1e
Werton <wertonf4bian@gmail.com>
2023-09-18 22:09:46 -0300

    hight quality audio

--- server/download.js
+++ server/download.js
@@ -6,7 +6,7 @@ export const download = (videoId) =>
     const videoURL = "https://www.youtube.com/shorts/" + videoId
     console.log("Realizando o download do video:", videoId)
 
-    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+    ytdl(videoURL, { quality: "highestaudio", filter: "audioonly" })
       .on("info", (info) => {
         const seconds = info.formats[0].approxDurationMs / 1000
 
